c41bb868d037adac00b2d73cead98be13bcd8025
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-01 10:32:51 +0100

    eslint

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,8 @@
-export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+export default function taskBlock() {
+  const task = false;
+  const task2 = true;
 
-  if (trueOrFalse) {
+  if (true) {
     const task = true;
     const task2 = false;
   }
