09cbd0960d1824a7b87e6f3f80dcaa824a852eb9
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-01 10:36:07 +0100

    setup code to meet eslint standard

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock() {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (true) {
-    const task = true;
-    const task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
