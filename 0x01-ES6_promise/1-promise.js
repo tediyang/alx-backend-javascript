b080bed8d197aaf400c753247e6def6a7dedf7ef
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-03 21:13:46 +0100

    handle eslint

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,13 +2,12 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     const data = {
       status: 200,
-      body: 'Success'
-    }
+      body: 'Success',
+    };
 
     if (success) {
       resolve(data);
-    }
-    else {
+    } else {
       reject(Error('The fake API is not working currently'));
     }
   });
