6d8d248d4f9f72fcbcc1d507bc7a27da937c3f28
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-03 20:56:51 +0100

    if condition with promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    const data = {
+      status: 200,
+      body: 'Success'
+    }
+
+    if (success) {
+      resolve(data);
+    }
+    else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
