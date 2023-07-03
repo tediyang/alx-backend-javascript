204be4eea8daa012a1b82201b31dc7d50e2eb1e1
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-03 22:14:02 +0100

    using then for promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    const data = {
-      status: 200,
-      body: 'Success',
-    };
-
-    if (promise) {
-      resolve(data);
-    } else {
-      reject(Error());
-    }
-
-  }).then(console.log('Got a response from the API'));
+  return promise
+    .then(() => ({ status: 200, body: "Success" }))
+    .catch(() => Error())
+    .then(() => (console.log("Got a response from the API")));
 }
