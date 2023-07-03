2a72731f1b6518516e93e340c0901055b96f0d7e
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-03 23:14:13 +0100

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,11 +2,12 @@ export default function handleResponseFromAPI(promise) {
   const data = {
     status: 200,
     body: 'Success',
-  }
+  };
 
   promise
     .then(
       () => data,
-      () => new Error())
-    .finally(() => console.log("Got a response from the API"))
+      () => new Error()
+    )
+    .finally(() => console.log('Got a response from the API'));
 }
