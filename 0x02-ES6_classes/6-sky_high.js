dccf1e9a946854cc30622a00537f63ebb42f7464
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-05 20:53:41 +0100

    sky high

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,35 @@
+import Building from "./5-building";
+
+export default class SkyHighBuilding extends Building{
+  constructor(sqft, floors) {
+    super(sqft);
+    this.sqft = sqft;
+    this.floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new Error('sqft must be a number');
+    }
+    this._sqft = value;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new Error('floors must be a number');
+    }
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
