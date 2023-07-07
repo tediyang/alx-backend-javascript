862fcb2c2c687b8d5c5928e5c1c28d62ea9fd183
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-07 22:53:10 +0100

    gloceries

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+  const gloceries = new Map();
+
+  gloceries.set('Apples', 10);
+  gloceries.set('Tomatoes', 10);
+  gloceries.set('Pasta', 1);
+  gloceries.set('Rice', 1);
+  gloceries.set('Banana', 5);
+
+  return gloceries;
+}
