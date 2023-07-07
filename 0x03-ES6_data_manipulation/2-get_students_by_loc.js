991d1f390219525ef12b3c9bd9f5225d856d8451
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-07 18:58:23 +0100

    get student by loc: using filter

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,10 +1,3 @@
 export default function getStudentsByLocation(arrayStudent, city) {
-  const students = [];
-  for (const student of arrayStudent) {
-    if (student.location === city) {
-      students.push(student);
-    } 
-  }
-
-  return students;
+  return arrayStudent.filter((student) => student.location == city);
 }
