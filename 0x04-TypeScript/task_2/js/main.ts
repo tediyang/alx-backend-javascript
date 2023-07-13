b640d229da74d7cb03d62693fa27290efd44fe51
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-13 20:40:20 +0100

    create function

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,48 @@
+export interface DirectorInterface {
+  workFromHome(): string,
+  getCoffeeBreak(): string,
+  workDirectorTasks(): string
+}
+
+export interface TeacherInterface {
+  workFromHome(): string,
+  getCoffeeBreak(): string,
+  workTeacherTasks(): string
+}
+
+export class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home'; 
+  };
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  };
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  };
+}
+
+export class Teacher implements TeacherInterface {
+  workFromHome(): string {
+      return 'Cannot work from home';
+  };
+
+  getCoffeeBreak(): string {
+      return 'Cannot have a break';
+  };
+
+  workTeacherTasks(): string {
+      return 'Getting to work';
+  };
+}
+
+export function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 5000 ) {
+    return new Teacher;
+  }
+  else {
+    return new Director;
+  }
+}
