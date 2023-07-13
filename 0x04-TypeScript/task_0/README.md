bf3f7acec213619f9bdfad6bb7cdee4a40d0b6db
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-13 15:10:37 +0100

    ts script that add student data to a table

---
+++ 0x04-TypeScript/task_0/README.md
@@ -0,0 +1,2 @@
+# task_0
+Contains the configuration files and the ts file that render student data to a table.
---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,39 @@
+// Create interface of student
+interface Student {
+  firstName: string,
+  lastName: string,
+  age: number,
+  location: string
+}
+
+// First student
+const student1: Student = {
+  firstName: 'Daniel',
+  lastName: 'Eyang',
+  age: 27,
+  location: 'Lagos'
+};
+
+// Second student
+let student2: Student = {
+  firstName: 'Reaper',
+  lastName: 'Codes',
+  age: 2,
+  location: 'Lagos'
+};
+
+// List of students
+const studentsList = [student1, student2]
+
+// Get the table from the html file
+const table = document.createElement('table') as HTMLTableElement;
+
+// Create a new row for each student and append it to the table
+studentsList.forEach((student) => {
+	const row = table.insertRow();
+	const firstNameCell = row.insertCell(0);
+	const locationCell = row.insertCell(1);
+
+	firstNameCell.innerHTML = student.firstName;
+	locationCell.innerHTML = student.location;
+})
