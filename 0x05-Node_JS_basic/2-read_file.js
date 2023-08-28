1928dac8fbb74be03269a4021c37e8252e4782f0
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-28 21:43:23 +0100

    added try catch for database not found case

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,23 +1,28 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  const data = fs.readFileSync(path, 'utf-8');
-  const readData = data.split('\n');
-  let csStud = 0;
-  let sweStud = 0;
-  const csFir = [];
-  const sweFir = [];
-
-  for (let i = 1; i < readData.length; i += 1) {
-    const studInfo = readData[[i]].split(',');
-    if (studInfo[3].replace(/\r/g, '') === 'CS') {
-      csStud += 1;
-      csFir.push(studInfo[0]);
-    } else {
-      sweStud += 1;
-      sweFir.push(studInfo[0]);
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const readData = data.split('\n');
+    let csStud = 0;
+    let sweStud = 0;
+    const csFir = [];
+    const sweFir = [];
+  
+    for (let i = 1; i < readData.length; i += 1) {
+      const studInfo = readData[[i]].split(',');
+      if (studInfo[3].replace(/\r/g, '') === 'CS') {
+        csStud += 1;
+        csFir.push(studInfo[0]);
+      } else {
+        sweStud += 1;
+        sweFir.push(studInfo[0]);
+      }
     }
+  } catch {
+    throw new Error('Cannot load the database');
   }
+
   console.log(`Number of students: ${csStud + sweStud}`);
   console.log(`Number of students in CS: ${csStud}. List: ${csFir
     .join(', ')}`);
