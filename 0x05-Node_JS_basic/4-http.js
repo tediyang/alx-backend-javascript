4b2663ebdc176c64c06d108e8adc83c01f70273e
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-28 22:52:44 +0100

    http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+app.listen(port, () => {
+console.log('...');
+});
