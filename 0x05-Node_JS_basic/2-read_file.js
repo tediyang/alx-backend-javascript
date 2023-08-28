0b53ee83c86db9c4dc0a13444c4cc250428375e4
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-28 21:36:31 +0100

    used readFileSync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,35 +1,28 @@
-const readCsv = require('fs');
+const fs = require('fs');
 
 function countStudents(path) {
-  readCsv.readFile(path, 'utf-8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
-    const readData = data.split('\n');
-    /* Since the file line contains the header */
-    const header = readData[0].split(',');
-    const studentNo = readData.length - 1;
-    let csStud = 0;
-    let sweStud = 0;
-    let csFir = [];
-    let sweFir = [];
+  data = fs.readFileSync(path, 'utf-8');
+  const readData = data.split('\n');
+  let csStud = 0;
+  let sweStud = 0;
+  const csFir = [];
+  const sweFir = [];
 
-    for (let i = 1; i < readData.length; i++) {
-      studInfo = readData[[i]].split(',');
-      if (studInfo[3].replace(/\r/g, '') === "CS") {
-        csStud++;
-        csFir.push(studInfo[0]);
-      } else {
-        sweStud++;
-        sweFir.push(studInfo[0]);
-      }
+  for (let i = 1; i < readData.length; i++) {
+    const studInfo = readData[[i]].split(',');
+    if (studInfo[3].replace(/\r/g, '') === 'CS') {
+      csStud += 1;
+      csFir.push(studInfo[0]);
+    } else {
+      sweStud += 1;
+      sweFir.push(studInfo[0]);
     }
-    console.log(`Number of students: ${studentNo}`);
-    console.log(`Number of students in CS: ${csStud}. List: ${csFir.
-      join(", ")}`);
-    console.log(`Number of students in SWE: ${sweStud}. List: ${sweFir.
-      join(", ")}`);
-  });
+  }
+  console.log(`Number of students: ${csStud + sweStud}`);
+  console.log(`Number of students in CS: ${csStud}. List: ${csFir
+    .join(", ")}`);
+  console.log(`Number of students in SWE: ${sweStud}. List: ${sweFir
+    .join(", ")}`);
 }
 
 module.exports = countStudents;
