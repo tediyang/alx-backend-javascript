48299e278068f8677cf418561de900d9e93d0de2
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-28 21:56:57 +0100

    my code that runs properly

---
+++ 0x05-Node_JS_basic/2-read_file_mine.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const readData = data.split('\n');
+    let csStud = 0;
+    let sweStud = 0;
+    const csFir = [];
+    const sweFir = [];
+
+    for (let i = 1; i < readData.length; i += 1) {
+      const studInfo = readData[[i]].split(',');
+      if (studInfo[3].replace(/\r/g, '') === 'CS') {
+        csStud += 1;
+        csFir.push(studInfo[0]);
+      } else {
+        sweStud += 1;
+        sweFir.push(studInfo[0]);
+      }
+    }
+    console.log(`Number of students: ${csStud + sweStud}`);
+    console.log(`Number of students in CS: ${csStud}. List: ${csFir
+      .join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStud}. List: ${sweFir
+      .join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
