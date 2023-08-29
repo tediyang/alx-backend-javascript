9f8658abd6c8f350eda55ac5514fd147dfde736d
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-29 01:02:11 +0100

    eslint

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -10,38 +10,37 @@ const VALID_MAJORS = ['CS', 'SWE'];
  * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
 class StudentsController {
-static getAllStudents(request, response) {
-  const dataPath = process.argv.length > 2 ? process.argv[2] : '';
+  static getAllStudents(request, response) {
+    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
+    readDatabase(dataPath)
+      .then((studentGroups) => {
+        const responseParts = ['This is the list of our students'];
+        // A comparison function for ordering a list of strings in ascending
+        // order by alphabetic order and case insensitive
+        const cmpFxn = (a, b) => {
+          if (a[0].toLowerCase() < b[0].toLowerCase()) {
+            return -1;
+          }
+          if (a[0].toLowerCase() > b[0].toLowerCase()) {
+            return 1;
+          }
+          return 0;
+        };
 
-  readDatabase(dataPath)
-    .then((studentGroups) => {
-      const responseParts = ['This is the list of our students'];
-      // A comparison function for ordering a list of strings in ascending
-      // order by alphabetic order and case insensitive
-      const cmpFxn = (a, b) => {
-        if (a[0].toLowerCase() < b[0].toLowerCase()) {
-          return -1;
+        for (const [field, group] of Object.entries(studentGroups).sort(cmpFxn)) {
+          responseParts.push([
+            `Number of students in ${field}: ${group.length}.`,
+            'List:',
+            group.map((student) => student.firstname).join(', '),
+          ].join(' '));
         }
-        if (a[0].toLowerCase() > b[0].toLowerCase()) {
-          return 1;
-        }
-        return 0;
-      };
-
-      for (const [field, group] of Object.entries(studentGroups).sort(cmpFxn)) {
-        responseParts.push([
-          `Number of students in ${field}: ${group.length}.`,
-          'List:',
-          group.map((student) => student.firstname).join(', '),
-        ].join(' '));
-      }
-      response.status(200).send(responseParts.join('\n'));
-    })
-    .catch((err) => {
-      response
-        .status(500)
-        .send(err instanceof Error ? err.message : err.toString());
-    });
+        response.status(200).send(responseParts.join('\n'));
+      })
+      .catch((err) => {
+        response
+          .status(500)
+          .send(err instanceof Error ? err.message : err.toString());
+      });
 }
 
 static getAllStudentsByMajor(request, response) {
