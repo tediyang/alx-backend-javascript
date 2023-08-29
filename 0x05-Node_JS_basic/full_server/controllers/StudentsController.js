adc3a87c9604ee674a4c255e57e6551f8139fdcf
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-29 01:05:37 +0100

    eslint

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -41,32 +41,32 @@ class StudentsController {
           .status(500)
           .send(err instanceof Error ? err.message : err.toString());
       });
-}
-
-static getAllStudentsByMajor(request, response) {
-  const dataPath = process.argv.length > 2 ? process.argv[2] : '';
-  const { major } = request.params;
-
-  if (!VALID_MAJORS.includes(major)) {
-    response.status(500).send('Major parameter must be CS or SWE');
-    return;
   }
-  readDatabase(dataPath)
-    .then((studentGroups) => {
-      let responseText = '';
 
-      if (Object.keys(studentGroups).includes(major)) {
-        const group = studentGroups[major];
-        responseText = `List: ${group.map((student) => student.firstname).join(', ')}`;
-      }
-      response.status(200).send(responseText);
-    })
-    .catch((err) => {
-      response
-        .status(500)
-        .send(err instanceof Error ? err.message : err.toString());
-    });
-}
+  static getAllStudentsByMajor(request, response) {
+    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
+    const { major } = request.params;
+
+    if (!VALID_MAJORS.includes(major)) {
+      response.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+    readDatabase(dataPath)
+      .then((studentGroups) => {
+        let responseText = '';
+
+        if (Object.keys(studentGroups).includes(major)) {
+          const group = studentGroups[major];
+          responseText = `List: ${group.map((student) => student.firstname).join(', ')}`;
+        }
+        response.status(200).send(responseText);
+      })
+      .catch((err) => {
+        response
+          .status(500)
+          .send(err instanceof Error ? err.message : err.toString());
+      });
+  }
 }
 
 export default StudentsController;
