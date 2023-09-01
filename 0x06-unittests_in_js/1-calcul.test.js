93b364fd0a43d0783fa020fc3264e85b56b73fc7
Nnanami Ajuluchukwu Joel <daniel.eyang@yahoo.com>
2023-09-01 12:50:00 +0100

    remove the describe calcnumber

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,53 +2,55 @@ const calculateNumber = require('./1-calcul.js');
 const assert = require('assert');
 
 
+describe('calculate Number', function () {
+    describe('#SUM', function () {
+        it('should round and return the sum', function () {
+            const a = 1.6;
+            const b = 2;
+            const expectedVal = 4;
 
-describe('SUM', function () {
-    it('should round and return the sum', function () {
-        const a = 1.6;
-        const b = 2;
-        const expectedVal = 4;
+            const theSum =  calculateNumber('SUM', a, b);
 
-        const theSum =  calculateNumber('SUM', a, b);
-
-        assert.strictEqual(expectedVal, theSum);
-    });
-});
-
-
-
-describe('SUBTRACT', function () {
-    it('should round and subtract', function () {
-        const a = 3;
-        const b = 1.6;
-        const expectedVal = 1;
-
-        const theSubtracted = calculateNumber('SUBTRACT', a, b);
-
-        assert.strictEqual(expectedVal, theSubtracted);
-    });
-});
-
-
-describe('DIVIDE', function () {
-    describe('if denominator rounds to 0', function () {
-        it('should return Error', function (){
-            const a = 5;
-            const b = 0.3;
-            const expectedVal = 'Error';
-
-            const theDivided = calculateNumber('DIVIDE', a, b);
-
-            assert.strictEqual(expectedVal, theDivided);
+            assert.strictEqual(expectedVal, theSum);
         });
-        it('should return the divided value', function () {
-            const a = 5;
-            const b = 0.7;
-            const expectedVal = 5;
+    });
+});
 
-            const theDivided = calculateNumber('DIVIDE', a, b);
+describe('calculate Number', function () {
+    describe('#SUBTRACT', function () {
+        it('should round and subtract', function () {
+            const a = 3;
+            const b = 1.6;
+            const expectedVal = 1;
 
-            assert.strictEqual(expectedVal, theDivided);
+            const theSubtracted = calculateNumber('SUBTRACT', a, b);
+
+            assert.strictEqual(expectedVal, theSubtracted);
+        });
+    });
+});
+
+describe('calculate Number', function () {    
+    describe('#DIVIDE', function () {
+        describe('if denominator rounds to 0', function () {
+            it('should return Error', function (){
+                const a = 5;
+                const b = 0.3;
+                const expectedVal = 'Error';
+
+                const theDivided = calculateNumber('DIVIDE', a, b);
+
+                assert.strictEqual(expectedVal, theDivided);
+            });
+            it('should return the divided value', function () {
+                const a = 5;
+                const b = 0.7;
+                const expectedVal = 5;
+
+                const theDivided = calculateNumber('DIVIDE', a, b);
+
+                assert.strictEqual(expectedVal, theDivided);
+            });
         });
     });
 });
