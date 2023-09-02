072b4b8cd7cf28c6a5a71569272a89d62d22e9ab
Nnanami Ajuluchukwu Joel <daniel.eyang@yahoo.com>
2023-09-02 17:19:54 +0100

    test for id

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -4,15 +4,16 @@ const app = express();
 const port = 7865;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the payment system');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id', (req, res) => {
-    res.send(`Payment methods for cart :${id}`);
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  console.log(`${req.params.id}`)
+  res.end(`Payment methods for cart ${req.params.id}`);
 })
 
-app.listen(port, () => {
-    console.log(`API available on localhost port ${port}`);
-});
+app.listen(port, console.log(`API available on localhost port ${port}`));
 
-module.exports = app;
+module.exports = [app, port];
